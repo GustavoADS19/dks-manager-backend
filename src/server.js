15b1f9e092214df9ac303590053600e82e00eaf5
Matth33w <matheus.ibrahim@hotmail.com>
2021-02-01 16:50:52 -0300

    Cors update

--- src/server.js
+++ src/server.js
@@ -15,9 +15,9 @@ const database = require("./database/connection");
 const port = process.env.PORT || 3333;
 
 //Middlewares (express)
-app.use(cors());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json({limit: '50mb'}));
+app.use(cors());
 app.use(routes);
 
 app.listen(port, console.log(`App listening on ${port}.`));
